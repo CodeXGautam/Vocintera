c77585b9f29195f40bc0474cb9bfe0567fd35046
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-01 19:00:04 +0530

    Refactor Interview, Practice, and Settings components to pass loggedIn and setLoggedIn props to Sidebar

--- frontend/src/Pages/Interview.js
+++ frontend/src/Pages/Interview.js
@@ -2,10 +2,14 @@ import Footer from "../Components/Footer";
 import Sidebar from "../Components/Sidebar";
 
 
-const Interview = () => {
+const Interview = (props) => {
+
+    const loggedIn = props.loggedIn;
+    const setLoggedIn = props.setLoggedIn;
+
     return (
         <div className="flex w-[100%] relative gap-4 overflow-hidden">
-            <Sidebar />
+            <Sidebar loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>
 
             <div className="w-[100%] max-h-screen bg-slate-900 flex flex-col gap-8 transition-all
             duration-300 ease-in-out rounded-md shadow-md shadow-blue-500 p-5 overflow-y-scroll">
