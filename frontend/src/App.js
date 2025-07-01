59f9c67df6fc4000c4ea6cb3e1aca45a86842cfc
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-01 18:14:54 +0530

    Add Interview and Settings pages with routing updates in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import RegisterPage from './Pages/RegisterPage';
 import LoginPage from './Pages/LoginPage';
 import Home from './Pages/Home';
 import Practice from './Pages/Practice';
+import Interview from './Pages/Interview';
+import Settings from './Pages/Settings';
 
 
 const App = () => {
@@ -17,16 +19,24 @@ const App = () => {
     <div className="bg-slate-950 min-h-screen flex flex-col relative">
 
       <Routes>
-        <Route path='/' element={<LandingPage loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>} />
-        <Route path='/register' element={<RegisterPage loggedIn={loggedIn} setLoggedIn={setLoggedIn}  />} />
-        <Route path='/login' element={<LoginPage  loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+        <Route path='/' element={<LandingPage loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+        <Route path='/register' element={<RegisterPage loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+        <Route path='/login' element={<LoginPage loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
         {
           loggedIn &&
-          <Route path='/home' element={<Home loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>}/>
+          <Route path='/home' element={<Home loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
         }
-         {
+        {
           loggedIn &&
-          <Route path='/practice' element={<Practice loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>}/>
+          <Route path='/practice' element={<Practice loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+        }
+        {
+          loggedIn &&
+          <Route path='/interview' element={<Interview loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+        }
+        {
+          loggedIn &&
+          <Route path='/settings' element={<Settings loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
         }
       </Routes>
     </div>
