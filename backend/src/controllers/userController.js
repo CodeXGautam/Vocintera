934e2bff857383823000132f086153e3327bce69
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-02 13:04:33 +0530

    Fix user login query to use findOne method for accurate email lookup

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -94,7 +94,7 @@ const loginUser = async (req, res) => {
             return res.status(400).json({ message: "All fields are required" });
         };
 
-        const user = await User.findById({ email: email});
+        const user = await User.findOne({email : email});
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         };
