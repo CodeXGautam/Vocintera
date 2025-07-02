b87699c315f14c8db60fe6bf2470a29cc24572c1
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-02 10:20:23 +0530

    Add JWT verification middleware for user authentication

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+
+export const jwtVerify = async (req, res, next) => {
+    try {
+        console.log(req.cookies.accessToken)
+        const token = req.cookies?.accessToken;
+
+        if (!token || typeof token !== "string") {
+            return res.status(401).json({ message: "Unauthorized: No token provided" });
+        }
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decoded._id).select("-password -refreshToken");
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        req.user = user;
+        next();
+    }
+    catch (error) {
+        console.error("JWT verification error:", error.message);
+        if (error.name === "TokenExpiredError") {
+            return res.status(401).json({ message: "Token expired. Please login again." });
+        }
+        res.status(403).json({ message: "Forbidden: Invalid or expired token" });
+    }
+};
